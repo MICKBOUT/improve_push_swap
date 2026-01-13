9f2833455108d2b0a41c3d60aa030445a64f6434
MICKBOUT <matisse.boutte@gmail.com>
2026-01-13 15:01:56 +0100

    rm obj folder

--- .obj/ft_bucket.d
+++
@@ -1,5 +0,0 @@
-.obj/ft_bucket.o: ft_bucket.c push_swap.h push_swap_common.h
-
-push_swap.h:
-
-push_swap_common.h:
--- .obj/ft_bucket_utils.d
+++
@@ -1,5 +0,0 @@
-.obj/ft_bucket_utils.o: ft_bucket_utils.c push_swap.h push_swap_common.h
-
-push_swap.h:
-
-push_swap_common.h:
